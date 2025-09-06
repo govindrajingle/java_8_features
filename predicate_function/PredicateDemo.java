3266ed0eca67fcbd20f4601006cfe1a6e88534e3
Govindraj Ingle <govindrajingle@gmail.com>
2025-09-06 17:46:33 +0530

    predicate interface

---
+++ predicate_function/PredicateDemo.java
@@ -0,0 +1,23 @@
+package predicate_function;
+
+import java.util.function.Predicate;
+
+public class PredicateDemo {
+    // We have Predicate as a functional interface, meaning that only one abstract
+    // method
+    // (test()) and can refer the lambda expression.
+
+    // The test() method takes arguments and returns only boolean value.
+    public static void main(String[] args) {
+        int y = 100;
+        Predicate<Integer> isEven = x -> x % 2 == 0;
+        Predicate<Integer> isGreaterThan = x -> x >= 100;
+
+        // Predicate Chaining (combine and, or, negate)
+        Predicate<Integer> isEvenAndGreaterThan = isEven.and(isGreaterThan);
+
+        System.out.println("Is " + y + " Even Number : " + isEven.test(y));
+        System.out.println(
+                "Is " + y + " Greater Than or Equal to 100 and Even Number: " + isEven.and(isGreaterThan).test(y));
+    }
+}
